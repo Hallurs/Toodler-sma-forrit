08c590d6a19246743e8299025309fcd62aff8001
brynni <bryni97hotmail.com>
2021-11-23 10:11:48 +0000

    orginization hmm

---
+++ src/styles/colors.js
@@ -0,0 +1,1 @@
+export const periwinkle = '#CAE5FF';
---
+++ src/views/main/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text, TouchableHighlight, Image } from 'react-native';
+import styles from './styles';
+
+const Main = () => (
+    <View>
+        <h1> Welcome to toodler! </h1>
+        <TouchableHighlight>
+            <Text>Boards</Text>
+        </TouchableHighlight>
+
+    </View>
+);
+
+export default Main;
